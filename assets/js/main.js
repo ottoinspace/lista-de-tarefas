a67d456cc9120eb21bf5822009c7640a877e780a
Arnoldo Bezerra Pinheiro <arnoldobepi@gmail.com>
2023-06-26 14:29:52 -0300

    funcao de apagar tarefas

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,13 +7,44 @@ function criaLi(){
     return li;
 }
 
+inputTarefas.addEventListener('keypress', function(e){
+    if (e.keyCode === 13) {
+        if (!inputTarefas.value) return;
+        criaTarefa(inputTarefas.value);  
+    }
+})
+
+function limpaInput() {
+    inputTarefas.value = '';
+    inputTarefas.focus();
+}
+
+function criaBotaoApagar(li) {
+    li.innerText += ' ';
+    const botaoApagar = document.createElement('button');
+    botaoApagar.innerText = 'Apagar';
+    botaoApagar.setAttribute('class', 'apagar');
+    li.appendChild(botaoApagar);
+}
+
 function criaTarefa(textoInput){
     const li = criaLi();
     li.innerHTML = textoInput;
     tarefa.appendChild(li);
+    limpaInput();
+    criaBotaoApagar(li);
+    salvarTarefas
 }
 
 btnTarefa.addEventListener('click', function(e){
     if (!inputTarefas.value) return;
     criaTarefa(inputTarefas.value);
+});
+
+document.addEventListener('click', function(e){
+    const el = e.target;
+
+    if (el.classList.contains('apagar')) {
+        el.parentElement.remove();
+    }
 });
